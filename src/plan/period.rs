6989006b145865da93bf59a5f16093483ec3a957
Javier Albero <splash.creeper1789@gmail.com>
2025-01-28 22:42:31 +0100

    Line break escapes removed

--- src/plan/period.rs
+++ src/plan/period.rs
@@ -15,10 +15,10 @@ const FORMAT : &str = "%m-%d-%Y";
 fn display_bad_usage() {
     println!(
         "Bad usage: {} plan ...:\n|
-        - start [start] (end) (description) : Starts a new study plan. It starts today if no start date is provided.\n\
+        - start [start] (end) (description) : Starts a new study plan. It starts today if no start date is provided.
         - list : Lists all the study periods.
-        - modify [--plan (plan id)] [--start (new start date)] [--end (new end date)] [--description (new description)] : Modifies the current plan (or one determined by an id).\n\
-        - remove [--plan (plan id)] [--confirm] : Removes the actual study plan (or one determined by id). Use the --confirm option to do so without any warning.\n\
+        - modify [--plan (plan id)] [--start (new start date)] [--end (new end date)] [--description (new description)] : Modifies the current plan (or one determined by an id).
+        - remove [--plan (plan id)] [--confirm] : Removes the actual study plan (or one determined by id). Use the --confirm option to do so without any warning.
         The date format is: {FORMAT}\n\
     ", crate::env::args().collect::<Vec<String>>().first().unwrap());
 }
