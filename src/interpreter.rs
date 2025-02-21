25d5a17c19218e0426f5a86024e98e5602bdeeb2
splashling <splashling1789@gmail.com>
2025-02-21 12:26:39 +0100

    Interpreter documented

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -1,7 +1,15 @@
+//! Main interpreter
+//! This module interprets the arguments given by the user and delegates 
+//! the work to command submodules ([plan], [subject], [export]). It also provides
+//! useful functions to every command submodule.
+
 use crate::{debug_println, plan, usage};
 use diesel::{Connection, SqliteConnection};
 use std::{env, process};
 
+/// Interprets the first command of the arguments provided and delegates the work to submodule commands
+/// # Arguments
+/// * `args` - Program arguments.
 pub fn interpret(args: &mut Vec<String>) {
     match args.len() {
         0 => {
@@ -23,6 +31,13 @@ pub fn interpret(args: &mut Vec<String>) {
         }
     }
 }
+
+/// It searchs the value of the argument provided.
+/// # Precondition:
+/// There must be an element which content equals the `find` content.
+/// # Arguments
+/// * `args` - program arguments
+/// * `find` - argument flag to find
 pub fn get_specific_arg(args: &mut Vec<String>, find : &str) -> Option<String> {
     args
         .get(args.iter().enumerate().find(|a| a.1 == find).unwrap().0 + 1)
