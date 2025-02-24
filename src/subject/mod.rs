0dc75a0299b37cfb90e05312fe78a518bd2f1875
splashling <splashling1789@gmail.com>
2025-02-24 12:18:03 +0100

    Created fetch_all_subjects function

--- src/subject/mod.rs
+++ src/subject/mod.rs
@@ -1,3 +1,19 @@
+use std::process;
+use diesel::{RunQueryDsl, SqliteConnection};
+use crate::models::Subject;
+use crate::schema::subjects::dsl::subjects;
+
 mod usage;
 mod interpreter;
 mod add;
+mod modify;
+
+fn fetch_all_subjects(conn : &mut SqliteConnection) -> Vec<Subject> {
+    match subjects.load::<Subject>(conn) {
+        Ok(result) => result,
+        Err(e) => {
+            eprintln!("Failed to fetch subjects: {}", e);
+            process::exit(1);
+        }
+    }
+}
