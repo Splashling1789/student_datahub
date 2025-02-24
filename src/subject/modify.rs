14e3d64a81c664817bfd6cf06b4d06157a8387c9
splashling <splashling1789@gmail.com>
2025-02-24 12:49:39 +0100

    Modify finished

---
+++ src/subject/modify.rs
@@ -0,0 +1,48 @@
+use std::process;
+use diesel::{update, QueryDsl, RunQueryDsl, SqliteConnection};
+use crate::interpreter::get_specific_arg;
+use crate::plan::interpreter::get_plan_arg;
+use crate::schema::entry::subject_id;
+use crate::schema::subjects::dsl::subjects;
+use crate::schema::subjects::{name, short_name};
+use crate::subject::interpreter::get_subject;
+use crate::subject::usage::display_bad_usage;
+
+pub fn modify(args : &mut Vec<String>, conn : &mut SqliteConnection) {
+    if(args.len() < 2) {
+        display_bad_usage();
+        process::exit(1);
+    }
+    let plan_id = get_plan_arg(args);
+    let subj = match get_subject(args.get(0).unwrap(), conn, Some(plan_id)) {
+        Some(subj) => subj,
+        None => {
+            eprintln!("Failed to get subject. Does this subject exist?");
+            process::exit(1);
+        }
+    };
+    let new_short_name = match get_specific_arg(args, "--short-name") {
+        Some(short) => short,
+        None => subj.short_name.clone()
+    };
+    // Two subjects from the same plan can't have the same short name.
+    if super::fetch_all_subjects(conn).iter().any(|s| s.period_id == subj.period_id && s.short_name.eq(&new_short_name)) {
+        eprintln!("A subject already exists in the period with the same short name.");
+        process::exit(1);
+    }
+    let new_name = match get_specific_arg(args, "--name") {
+        Some(n) => n,
+        None => subj.name.clone()
+    };
+
+    match update(subjects.filter(subject_id.eq(subj.id))).set((short_name.eq(new_short_name), name.eq(new_name))).execute(conn) {
+        Ok(_) => {
+            println!("Subjects edited succesfully.");
+        }
+        Err(e) => {
+            eprintln!("Failed to update the subject: {}", e);
+            process::exit(1);
+        }
+    }
+
+}
